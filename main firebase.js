75eb2970020e3673b1115c220e757e14bd28335c
S-Vairon <shirley.moirin@gmail.com>
2016-06-30 16:43:20 -0700

    Delete main firebase.js

--- main firebase.js
+++
@@ -1,9 +0,0 @@
-var myDBReference = new Firebase('https://summer-outsideconcerts-board.firebaseio.com');
-
-var config = {
-    apiKey: "AIzaSyCr92WuVPfwZDt5XpaK13BoJmcMz3C1hik",
-    authDomain: "summer-outsideconcerts-board.firebaseapp.com",
-    databaseURL: "https://summer-outsideconcerts-board.firebaseio.com",
-    storageBucket: "summer-outsideconcerts-board.appspot.com",
-  };
-  firebase.initializeApp(config);
